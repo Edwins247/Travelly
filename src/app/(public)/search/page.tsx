9827e02c13f113dd0aea43c5001149bdda12a8ae
Edwins247 <cheewr85@daum.net>
2025-07-07 15:33:09 +0900

    chore: Vercel 에러 추가 해결

--- src/app/(public)/search/page.tsx
+++ src/app/(public)/search/page.tsx
@@ -1,20 +1,20 @@
 // src/app/(public)/search/page.tsx
+// ToDo: 추후 SSR로 변경될 수 있음
+'use client';           
+
+import { useSearchParams } from 'next/navigation';
 import { FilterBar } from '@/components/search/FilterBar';
 import { PlaceGrid }  from '@/components/home/PlaceGrid';
 import { demoPlaces } from '@/mocks/places';
 
-interface Props {
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export default function SearchPage({ searchParams }: Props) {
-  const keyword = searchParams.keyword ?? '';
+export default function SearchPage() {
+  const params   = useSearchParams();
+  const keyword  = params.get('keyword') ?? '';
 
   return (
     <main className="mx-auto max-w-6xl space-y-6 px-4 py-10">
       <h1 className="text-xl font-semibold">“{keyword}” 검색 결과</h1>
 
-      {/* FilterBar 는 이미 'use client' 지시자가 있으므로 그대로 사용 */}
       <FilterBar />
 
       <PlaceGrid title="" places={demoPlaces} />
